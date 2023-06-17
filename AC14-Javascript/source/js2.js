429b05768bd75ac21eb86b47a3dab604844407fe
Weber <vito588p@gmail.com>
2023-06-17 13:04:51 +0800

    finish Js homework 1 FtoC CtoF

--- AC14-Javascript/source/js2.js
+++ AC14-Javascript/source/js2.js
@@ -1,2 +1,36 @@
 
+const transBtn = document.querySelector(".btn1");
+const resetBtn = document.querySelector(".btn2");
+const fahV = document.querySelector("#fahbox");
+const celV = document.querySelector("#celbox");
+
+fahV.addEventListener("focus", () => {
+    celV.value = ""
+})
+
+celV.addEventListener("focus", () => {
+    fahV.value = ""
+})
+
+transBtn.addEventListener("click", () => {
+    if(fahV.value == "" && celV.value == ""){
+        alert("請輸入至少一組數字！");
+    } else if(fahV.value && celV.value == false){
+        let fv = Number(fahV.value);
+        celV.value = ((fv - 32) * 5 / 9).toFixed(1);    
+    } else if(celV.value && fahV.value == false){
+        let cv = Number(celV.value);
+        fahV.value = (cv * 9 / 5 + 32).toFixed(1);
+    }  
+}) 
+
+resetBtn.addEventListener("click", () => {
+    fahV.value = "";
+    celV.value = "";
+})
+
+    
+
+
+    
 
